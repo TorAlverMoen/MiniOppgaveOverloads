24909cffaf9df49beb1a586d0f160a7e8c450f83
Tor Alver Moen <tor.alver.moen@gmail.com>
2023-11-16 11:01:53 +0100

    Initial commit

---
+++ MiniOppgaveOverloads/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OverloadTest
+{
+    internal class Program
+    {
+        static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Hei og velkommen.");
+        }
+
+        static void PrintWelcomeMessage(string name)
+        {
+            Console.WriteLine($"Hei {name} og velkommen.");
+        }
+
+        static void Main()
+        {
+            PrintWelcomeMessage("Tor");
+        }
+    }
+}
